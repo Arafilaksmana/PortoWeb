5fe3b0bfce2b6a8718db57d5a24f492f1b28d710
Dirga <arfilaks.dev@gmail.com>
2025-05-05 00:41:57 +0700

    ~

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 function Home() {
+  const fadeInUp = {
+    initial: { y: 20, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+    transition: { duration: 0.5, ease: "easeOut" },
+  };
+
   return (
     <div className="px-3 lg:px-8 pt-4">
       <div className="flex flex-col">
+        {/* Header */}
         <div className="flex h-[20dvh] lg:h-[40dvh]">
           <div className="w-1/2 lg:w-1/3">Dirga</div>
           <div className="w-1/2 lg:w-1/3 flex justify-end lg:justify-start">
@@ -11,24 +21,44 @@ function Home() {
             </span>
           </div>
         </div>
+
+        {/* Konten dengan animasi muncul dari wadah */}
         <div className="flex flex-col lg:flex-row gap-6 lg:gap-0">
-          <div className="flex flex-col w-full lg:w-1/3">
-            <span>Get in touch</span>
-            <span className="text-black/50">Arfilaks.dev@gmail.com</span>
+          {/* Kiri */}
+          <div className="flex flex-col w-full lg:w-1/3 space-y-2">
+            <div className="overflow-hidden">
+              <motion.div {...fadeInUp}>
+                <span>Get in touch</span>
+              </motion.div>
+            </div>
+            <div className="overflow-hidden">
+              <motion.div {...fadeInUp} transition={{ delay: 0.1, duration: 0.5 }}>
+                <span className="text-black/50">Arfilaks.dev@gmail.com</span>
+              </motion.div>
+            </div>
           </div>
+
+          {/* Kanan */}
           <div className="w-full lg:w-2/3">
-            <span className="text-5xl">
-              I am
-              <div className="w-[100px] h-14 inline-block align-middle mx-2">
-                <img
-                  src="PROPICT.jpg"
-                  alt="Dirga"
-                  className="w-full h-full object-contain rounded-full"
-                />
-              </div>
-              Dirga. A Frontend & Designer who creates solution based on user
-              needs.
-            </span>
+            <div className="overflow-hidden">
+              <motion.div
+                {...fadeInUp}
+                transition={{ delay: 0.2, duration: 0.6 }}
+              >
+                <span className="text-5xl tracking-tight">
+                  I am
+                  <div className="w-[100px] h-14 inline-block align-middle mx-2">
+                    <img
+                      src="PROPICT.jpg"
+                      alt="Dirga"
+                      className="w-full h-full object-contain rounded-full"
+                    />
+                  </div>
+                  Dirga. A Frontend & Designer who creates solution based on
+                  user needs.
+                </span>
+              </motion.div>
+            </div>
           </div>
         </div>
       </div>
