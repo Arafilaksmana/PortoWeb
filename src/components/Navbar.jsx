253773c7bb71fde9a6c261312c16b0e53e423640
Dirga <arfilaks.dev@gmail.com>
2025-05-04 15:14:09 +0700

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar() {
       {/* Navbar Container with Scale Animation */}
       <motion.div
         ref={navbarRef}
-        className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50 box-border bg-[#000000CC] text-white rounded-[32px] px-2 py-2 shadow-md min-w-[220px]"
+        className="fixed bottom-8 left-1/2 transform -translate-x-1/2 z-50 box-border bg-[#000000CC] text-white rounded-[32px] px-3 py-3 shadow-md min-w-[200px]"
         initial={false}
         animate={{
           scale: isExpanded ? 1.05 : 1,
