c019c004e24bca204de2fe19f1bb224f0d6ff8b3
Dirga <arfilaks.dev@gmail.com>
2025-05-11 16:51:07 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 function Home() {
   return (
     <>
-      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-screen w-full flex">
+      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-dvh w-full flex">
         <div className="text-white text-3xl leading-9 ml-3 lg:ml-auto mr-auto lg:mr-[20%] lg:my-auto self-end pb-12 lg:pb-0">
           {/* Bungkus tiap baris teks dengan overflow-hidden agar terlihat seperti muncul dari bawah kontainer */}
           <div className="overflow-hidden">
