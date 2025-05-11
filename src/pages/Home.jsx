01b114c7fa379d2b204888dfc6cd501c062c71d0
Dirga <arfilaks.dev@gmail.com>
2025-05-11 16:50:05 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
+
 function Home() {
-  const [bgLoaded, setBgLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = "/arafi.png";
-    img.onload = () => setBgLoaded(true);
-  }, []);
-
   return (
     <>
-      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat min-h-[100svh] w-full flex">
+      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-screen w-full flex">
         <div className="text-white text-3xl leading-9 ml-3 lg:ml-auto mr-auto lg:mr-[20%] lg:my-auto self-end pb-12 lg:pb-0">
+          {/* Bungkus tiap baris teks dengan overflow-hidden agar terlihat seperti muncul dari bawah kontainer */}
           <div className="overflow-hidden">
             <motion.div
               initial={{ y: "100%", opacity: 0 }}
-              animate={bgLoaded ? { y: "0%", opacity: 1 } : {}}
+              animate={{ y: "0%", opacity: 1 }}
               transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
             >
               I'm Dirga
@@ -26,7 +19,7 @@ function Home() {
           <div className="overflow-hidden">
             <motion.div
               initial={{ y: "100%", opacity: 0 }}
-              animate={bgLoaded ? { y: "0%", opacity: 1 } : {}}
+              animate={{ y: "0%", opacity: 1 }}
               transition={{ duration: 0.8, ease: "easeOut", delay: 0.4 }}
             >
               Frontend & Designer
