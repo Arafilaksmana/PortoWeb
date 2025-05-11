0b625b98b1e14d66e2500b1242365d4c083fb213
Dirga <arfilaks.dev@gmail.com>
2025-05-11 16:55:14 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,10 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+
 
 function Home() {
-  const [vh, setVh] = useState("100vh");
-
-  useEffect(() => {
-    const updateVh = () => {
-      setVh(`${window.innerHeight}px`);
-    };
-    updateVh();
-
-    window.addEventListener("resize", updateVh);
-    return () => window.removeEventListener("resize", updateVh);
-  }, []);
-
   return (
     <>
-      <div
-        style={{ height: vh }}
-        className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat w-full flex"
-      >
+      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-screen w-full flex">
         <div className="text-white text-3xl leading-9 ml-3 lg:ml-auto mr-auto lg:mr-[20%] lg:my-auto self-end pb-12 lg:pb-0">
           {/* Bungkus tiap baris teks dengan overflow-hidden agar terlihat seperti muncul dari bawah kontainer */}
           <div className="overflow-hidden">
