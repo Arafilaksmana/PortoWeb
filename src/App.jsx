d5482688ed945eb70b52204faa6f746519f87f5b
Dirga <arfilaks.dev@gmail.com>
2025-05-11 17:33:50 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </Router>
   );
