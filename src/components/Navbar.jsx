6db382f9af8f0c30f229eec9de8ce2a9a3ccb704
Dirga <arfilaks.dev@gmail.com>
2025-05-11 14:18:35 +0700

    p

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
       <div className="flex justify-between p-6 fixed top-0 left-0 z-10 w-full text-white">
         <div>Dirga</div>
         <div>
-          <div className="px-3 py-0.5">
+          <div className="px-3 py-0.5 bg-gray-400/10 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
             <a href="">Lets Talk</a>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,11 @@ function Home() {
   return (
     <>
       <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-dvh w-full flex items-center">
-        <div className="text-white text-2xl leading-7 ml-auto mr-auto">
+        <div className="text-white text-2xl leading-7 ml-auto mr-auto lg:mr-[25%] mt-0 lg:mt-[10%]">
           I'm Dirga <br /> Frontend & Designer
         </div>
       </div>
+      <div className="h-dvh"></div>
     </>
   );
 }
