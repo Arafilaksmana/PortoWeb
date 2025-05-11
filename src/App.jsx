d4ed8e8e966793e1910978cd3c7a891171adcaf3
Dirga <arfilaks.dev@gmail.com>
2025-05-11 17:30:14 +0700

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";  // Impor AnimatePresence
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home.jsx";
-import GreetingPage from "./pages/GreetingPage.jsx";
-import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import { useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+
+
 
 function App() {
-  useEffect(() => {
+
+    useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2, // semakin besar = scroll lebih lambat
+      duration: 1.2,       // semakin besar = scroll lebih lambat
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
-    });
+    })
 
     function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
+      lenis.raf(time)
+      requestAnimationFrame(raf)
     }
 
-    requestAnimationFrame(raf);
+    requestAnimationFrame(raf)
 
     return () => {
-      lenis.destroy();
-    };
-  }, []);
+      lenis.destroy()
+    }
+  }, [])
 
   return (
     <Router>
-      <Navbar />
-      <AnimatePresence>
-        <Routes>
-          <Route path="/" element={<GreetingPage />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </AnimatePresence>
+      <Navbar/>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+      </Routes>
     </Router>
   );
 }
 
-export default App;
+
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 
-
 function Home() {
   return (
     <>
