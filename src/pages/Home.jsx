c4a35824bf131628ca245b7b27c9d09b61248f86
Dirga <arfilaks.dev@gmail.com>
2025-05-11 14:30:43 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat h-dvh w-full flex">
+      <div className="bg-[url('/arafi.png')] bg-cover bg-top bg-no-repeat min-h-[100svh] w-full flex">
         <div className="text-white text-3xl lg:text-2xl leading-9 lg:leading-7 ml-3 lg:ml-auto mr-auto lg:mr-[25%] lg:my-auto self-end pb-12 lg:pb-0">
           I'm Dirga <br /> Frontend & Designer
         </div>
