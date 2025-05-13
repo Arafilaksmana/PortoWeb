7b10e8c99a865484c0b3cf0a60386f543511c808
Dirga <arfilaks.dev@gmail.com>
2025-05-13 10:51:55 +0700

    push

---
+++ src/components/ProjectDisplay.jsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+
+export default function ProjectDisplay({ project }) {
+  return (
+    <motion.div
+      key={project.title}
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+      className="mt-8 px-4 lg:px-8"
+    >
+      <div className="mt-8 px-4 lg:px-8">
+        <div className="w-full h-[600px]">
+          <img
+            src={project.image}
+            alt={project.title}
+            className="w-full h-full object-cover object-center rounded-xl"
+          />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-3 mt-4">
+          <div className="flex flex-col gap-2 flex-1/3 h-fi">
+            <div className="bg-white/7 rounded-lg px-4 py-2">
+              <h3 className="text-2xl">{project.title}</h3>
+            </div>
+            <div className="bg-white/7 rounded-lg px-4 py-2 flex items-center gap-4">
+              <h4 className="text-[18px]">Technology:</h4>
+              <div className="flex gap-4">{project.technologies}</div>
+            </div>
+          </div>
+          <div className="flex-1/3 bg-white/7 rounded-lg px-4 py-2">
+            <h4 className="text-[18px]">Description</h4>
+            <p className="font-light mt-4 pb-4">{project.description}</p>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import EntryOverlay from "../components/EntryOverlay.jsx";
+import ProjectToggle from "../components/ProjectToggle.jsx";
+import ProjectDisplay from "../components/ProjectDisplay.jsx";
+import { projects } from "../components/Projects.jsx";
 
 export default function Home() {
   const [scrollY, setScrollY] = useState(0);
   const [showOverlay, setShowOverlay] = useState(true);
   const [overlayExited, setOverlayExited] = useState(false);
+  const [currentProjectKey, setCurrentProjectKey] = useState("scholarium");
 
   const handleScroll = () => {
     setScrollY(window.scrollY);
@@ -30,6 +34,10 @@ export default function Home() {
   };
 
   const backgroundPosition = `center ${scrollY * 0.1}px`;
+
+  const handleProjectChange = (key) => {
+    setCurrentProjectKey(key);
+  };
 
   return (
     <>
@@ -139,48 +147,11 @@ export default function Home() {
         </h3>
 
         {/* TOGGLE SWITCH */}
-        <div className="flex justify-center mt-12">
-          <div className=" bg-[#171b1b] rounded-xl">
-            <button className="text-[16px] tracking-tight text-center w-[80px]  rounded-xl box-content px-4 py-2  bg-[#f4f4f5] text-black cursor-pointer">
-              Scholarium
-            </button>
-            <button className="text-[16px] tracking-tight text-center w-[80px]  rounded-xl box-content px-4 py-2 cursor-pointer">
-              Arte
-            </button>
-          </div>
-        </div>
-
-        {/* SHOWING CONTENT */}
-        <div className="mt-8 px-4 lg:px-8">
-          <div className="w-full h-[600px]">
-            <img
-              src="/scholarium.webp"
-              alt=""
-              className="w-full h-full object-cover object-center rounded-xl"
-            ></img>
-          </div>
-          <div className="flex flex-col lg:flex-row gap-3 mt-4">
-            <div className="flex-1/3 h-fit bg-white/7 rounded-lg px-4 py-2">
-              <h3 className="text-2xl">
-                Scholarium - Global Learning Excellence Journey
-              </h3>
-            </div>
-            <div className="flex-1/3 bg-white/7 rounded-lg px-4 py-2 ">
-              <h4 className="text-[18px]">Description</h4>
-              <p className="font-light mt-4 pb-4">
-                This project was created as an educational platform for students
-                who are looking for scholarships from various educational
-                institutions around the world. Here, they can browse and apply
-                for a wide range of scholarships offered by different
-                institutions.
-              </p>
-            </div>
-            <div className="flex-1/3 bg-white/7 rounded-lg px-4 py-2 ">
-              <h4 className="text-[18px]">Technology</h4>
-              <p className="font-light"></p>
-            </div>
-          </div>
-        </div>
+        <ProjectToggle
+          currentProjectKey={currentProjectKey}
+          onProjectChange={handleProjectChange}
+        />
+        <ProjectDisplay project={projects[currentProjectKey]} />
       </div>
     </>
   );
